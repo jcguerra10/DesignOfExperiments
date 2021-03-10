4790bf7b0f01bd94d8afe986c0521ad8e2406004
Julian A Rivera C <48961526+JulianAndresRiveraCarrillo@users.noreply.github.com>
2021-03-10 10:35:38 -0500

    Sort Methods implemented

--- DesignOfExperiments/model/OrderingMethod.cs
+++ DesignOfExperiments/model/OrderingMethod.cs
@@ -8,5 +8,39 @@ namespace DesignOfExperiments.model
 {
     class OrderingMethod
     {
+        public void insertionSort(int[] arr)    //Metodo de ordenamiento por insercion.
+        {
+            int n = arr.Length;
+            for (int i = 1; i < n; ++i)
+            {
+                int first = arr[i];
+                int j = i - 1;
+
+                while (j >= 0 && arr[j] > first)
+                {
+                    arr[j + 1] = arr[j];
+                    j = j - 1;
+                }
+                arr[j + 1] = first;
+            }
+        }
+
+        static void selectionSort(int[] arr)    //Metodo de ordenamiento por seleccion.
+        {
+            int n = arr.Length;
+
+            for (int i = 0; i < n - 1; i++)
+            {
+                int min = i;
+                for (int j = i + 1; j < n; j++)
+                {
+                    if (arr[j] < arr[min])
+                        min = j;
+                }
+                int temp = arr[min];
+                arr[min] = arr[i];
+                arr[i] = temp;
+            }
+        }
     }
 }
